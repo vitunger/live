d4541467991a28a1b7458e05d3608a8f38286193
vitunger <markus.unger@vitbikes.de>
2026-03-06 14:44:18 +0100

    feat: Entwicklung nutzung tab now calls renderApiNutzung instead of renderEntwNutzung

--- portal/views/dev-pipeline.js
+++ portal/views/dev-pipeline.js
@@ -128,7 +128,7 @@ export async function renderEntwTabContent(tab) {
     } else if(tab === 'flags') {
         if(typeof window.renderEntwFlags === 'function') window.renderEntwFlags();
     } else if(tab === 'nutzung') {
-        if(typeof window.renderEntwNutzung === 'function') window.renderEntwNutzung();
+        if(typeof window.renderApiNutzung === 'function') window.renderApiNutzung('entwNutzungContent');
     } else if(tab === 'system') {
         if(typeof window.renderEntwSystem === 'function') window.renderEntwSystem();
     } else if(tab === 'vision') {
