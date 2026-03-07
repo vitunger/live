f9ad6cd6974b791d386896bc20d1f2015d628e9d
vitunger <markus.unger@vitbikes.de>
2026-03-07 19:32:52 +0100

    chore: cache bust v=1772908372

--- portal/app.js
+++ portal/app.js
@@ -12,7 +12,7 @@
  */
 
 const MODULE_BASE = '/portal';
-const CACHE_BUST = "?v=1772908345";
+const CACHE_BUST = "?v=1772908372";
 // Read-only token for public repo (split to avoid secret scanning)
 window._GH_READ_TOKEN = ['ghp_kfihVZbz','AMZA','OcU3wPG0Pw8QWCF2UM0npLKj'].join('');
 
