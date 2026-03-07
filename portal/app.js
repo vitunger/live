f0b2d9266f70b19166970b7637505d6f9e0ec650
vitunger <markus.unger@vitbikes.de>
2026-03-07 12:04:22 +0100

    chore: cache bust for wissen filter fix + search

--- portal/app.js
+++ portal/app.js
@@ -12,7 +12,7 @@
  */
 
 const MODULE_BASE = '/portal';
-const CACHE_BUST = "?v=1772880853";
+const CACHE_BUST = "?v=1772881462";
 // Read-only token for public repo (split to avoid secret scanning)
 window._GH_READ_TOKEN = ['ghp_kfihVZbz','AMZA','OcU3wPG0Pw8QWCF2UM0npLKj'].join('');
 
